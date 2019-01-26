f6401de0b3ef0640081fb9aae233e6df92291d7b
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-01-26 10:02:33 -0200

    v0.3.0 - Adicionada conversão de arquivo xml para arquivo json

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var fs = require('fs');
+var fs = require('fs'),
+    convert = require('xml-js');
 
 exports = module.exports = AvaliacaoQualis;
 
 function AvaliacaoQualis(config, callback) {
-    if(!config.ClassificacoesPublicadas) {
+    if (!config.ClassificacoesPublicadas) {
         callback(new Error("Você não informou o arquivo xls de entrada."), null);
-    } else if(!config.output) {
+    } else if (!config.curriculoLattes) {
+        callback(new Error("Você não informou o arquivo xml de entrada."), null);
+    } else if (!config.output) {
         callback(new Error("Você não informou o nome do arquivo json de saída."), null);
-    }
+    } 
+    
     new Qualis(config, callback);
 }
 
 function Qualis(config, callback) { 
     this.xlsToJson(config.ClassificacoesPublicadas, config.output, callback);
+    this.xmlToJson(config.curriculoLattes);
 }
 
 Qualis.prototype.xlsToJson = function(ClassificacoesPublicadas, output) {
@@ -27,3 +32,21 @@ Qualis.prototype.xlsToJson = function(ClassificacoesPublicadas, output) {
     });
 }
 
+Qualis.prototype.xmlToJson = function (curriculoLattes) {
+    var xml = fs.readFileSync(curriculoLattes, 'utf8'),
+    options = {
+        ignoreComment: true, alwaysChildren: true, compact: true, addParent: true, spaces: 2
+    },
+    result = convert.xml2json(xml, options); 
+
+    fs.writeFile(
+        "curriculo-lattes.json", 
+        result, 
+        function(err) {
+          if(err) {
+            return console.log(err);
+          }
+          console.log("Arquivo convertido para formato json com sucesso.");
+        }  
+    ); 
+}
