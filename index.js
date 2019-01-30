005fd7e0160934e1281f06eb92169f360abfec7d
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-01-30 15:02:55 -0200

    Adicionado cruzamento de dados via ISSN e armazenamento de resultado em arquivo json

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 var fs = require('fs'),
-    convert = require('xml-js');
+    convert = require('xml-js'),
+    arquivoLattes, arquivoQualis, dadosArtigo = [], artigos, cont = 0, data = { artigos: [] },
+    jsonfile = require('jsonfile');
 
 exports = module.exports = AvaliacaoQualis;
 
 function AvaliacaoQualis(config, callback) {
-    if (!config.ClassificacoesPublicadas) {
+    if (!config.classificacoesPublicadas) {
         callback(new Error("Você não informou o arquivo xls de entrada."), null);
     } else if (!config.curriculoLattes) {
         callback(new Error("Você não informou o arquivo xml de entrada."), null);
-    } else if (!config.output) {
-        callback(new Error("Você não informou o nome do arquivo json de saída."), null);
-    } 
-    
+    }
+
     new Qualis(config, callback);
 }
 
 function Qualis(config, callback) { 
-    this.xlsToJson(config.ClassificacoesPublicadas, config.output, callback);
+    this.xlsToJson(config.classificacoesPublicadas, callback);
     this.xmlToJson(config.curriculoLattes);
+    this.informaProducao(config.anoInicial, config.anoFinal);
 }
 
-Qualis.prototype.xlsToJson = function(ClassificacoesPublicadas, output) {
-    require("sheet2json")(ClassificacoesPublicadas, response => {
+Qualis.prototype.xlsToJson = function(classificacoesPublicadas) {
+    require("sheet2json")(classificacoesPublicadas, response => {
         var json = JSON.stringify(response, null, " ");
-        fs.writeFile(output, json, (err) => {
+        arquivoQualis = "./classificacoes_publicadas.json";
+        fs.writeFile(arquivoQualis, json, (err) => {
           if (err) {
             console.error(err)
             return
@@ -39,14 +41,88 @@ Qualis.prototype.xmlToJson = function (curriculoLattes) {
     },
     result = convert.xml2json(xml, options); 
 
+    arquivoLattes = "./curriculo-lattes.json";
     fs.writeFile(
-        "curriculo-lattes.json", 
+        arquivoLattes, 
         result, 
         function(err) {
           if(err) {
             return console.log(err);
           }
-          console.log("Arquivo convertido para formato json com sucesso.");
         }  
     ); 
+}
+
+Qualis.prototype.informaProducao = function(anoInicial, anoFinal) {
+    jsonfile.readFile(arquivoLattes, function (err, obj) {
+        if (err) console.error(err)
+        retornaArtigosNoIntervalo(obj, anoInicial, anoFinal);
+        verificaQuantidadeArtigos();
+    });
+    jsonfile.readFile(arquivoQualis, function (err, dadosQualis) {
+        if (err) console.error(err)
+        verificaISSN(dadosQualis);
+    });        
+}
+
+function retornaArtigosNoIntervalo(obj, anoInicial, anoFinal) {
+    var artigo = {};
+
+    artigos = obj['CURRICULO-VITAE']['PRODUCAO-BIBLIOGRAFICA']['ARTIGOS-PUBLICADOS']['ARTIGO-PUBLICADO'];
+
+    for (var i in artigos) {
+        artigo.ano = artigos[i]['DADOS-BASICOS-DO-ARTIGO']["_attributes"]["ANO-DO-ARTIGO"];
+        if (anoInicial <= artigo.ano && anoFinal >= artigo.ano) {
+            artigo.titulo = artigos[i]['DADOS-BASICOS-DO-ARTIGO']["_attributes"]["TITULO-DO-ARTIGO"];
+            artigo.issn = artigos[i]['DETALHAMENTO-DO-ARTIGO']["_attributes"]["ISSN"];
+            var obj = {
+                'titulo': artigo.titulo,
+                'ano': artigo.ano,
+                'issn': artigo.issn
+            }
+            dadosArtigo.push(obj);
+        }
+        else {
+            cont++;
+        }
+    }    
+}
+
+function verificaQuantidadeArtigos() {
+    if (artigos.length === cont) {
+        console.log("Não há artigos para o intervalo informado.")
+    }
+}
+
+function verificaISSN(dadosQualis) {
+    var  periodico = {}, objArtigo = {};
+
+    for (var i in dadosArtigo) {
+        for (var j in dadosQualis) {
+            periodico.issn = dadosQualis[j]['ISSN'];
+            periodico.issn = periodico.issn.split('-').join('');
+            periodico.titulo = dadosQualis[j]['Título'];
+            periodico.conceito = dadosQualis[j]['Estrato'];
+            if (periodico.issn == dadosArtigo[i].issn) {
+                objArtigo = {
+                    'tituloDoArtigo': dadosArtigo[i].titulo,
+                    'anoDoArtigo': dadosArtigo[i].ano,
+                    'issnDoArtigo': dadosArtigo[i].issn,
+                    'tituloDoPeriodico': periodico.titulo, 
+                    'conceitoDoPeriodico': periodico.conceito
+                }
+                data.artigos.push(objArtigo);
+                salvaDadosdaProducaoemArquivo();
+                break;
+            }
+        }
+    }
+}
+
+function salvaDadosdaProducaoemArquivo() {
+    var json = JSON.stringify(data, null, " ");
+    fs.writeFile ("./artigos-com-estrato-por-periodico.json", json, function(err) {
+        if (err) throw err;
+        console.log("Arquivo com artigos e suas classificações gerado.");
+    });
 }
