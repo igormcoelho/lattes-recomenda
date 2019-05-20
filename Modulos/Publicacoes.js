959570371bc5a703c2f7dc101fe323adb17b6ca4
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-05-20 01:19:30 -0300

    Alterado publicações para uso da classe de parse

--- Modulos/Publicacoes.js
+++ Modulos/Publicacoes.js
@@ -1,8 +1,9 @@
 var fs = require('fs'),
-    convert = require('xml-js'),
-    sheet2json = require("sheet2json"),
-    jsonfile = require('jsonfile'),
-    arquivoLattes, arquivoQualis, dadosArtigo = [], artigos, cont = 0, data = { artigos: [] };
+    jsonLattesObj, jsonQualisObj,
+    dadosArtigo = [], artigos, cont = 0, data = { artigos: [] };
+
+
+const Parse = require('./ParseData');
 
 
 exports = module.exports.AvaliacaoPublicacao = AvaliacaoPublicacao
@@ -17,6 +18,7 @@ function AvaliacaoPublicacao(config, callback) {
     } else if (!config.curriculoLattes) {
 
         callback(new Error("Você não informou o arquivo xml de entrada."), null);
+    
     }
 
     new Publicacao(config, callback);
@@ -25,75 +27,38 @@ function AvaliacaoPublicacao(config, callback) {
 
 function Publicacao(config, callback) { 
 
-    this.xlsToJson(config.classificacoesPublicadas, callback);
-    this.xmlToJson(config.curriculoLattes);
-    this.informaProducao(config.anoInicial, config.anoFinal);
+    let parse = new Parse();
+
+    jsonLattesObj = parse.parseXmlToJson(config.curriculoLattes, callback);
+    jsonQualisObj = parse.parseXlsToJson(config.classificacoesPublicadas, callback);
+    
+    this.informaProducao(jsonLattesObj, config.anoInicial, config.anoFinal);
     this.salvaProducao();
 }
 
 
-Publicacao.prototype.xlsToJson = function(classificacoesPublicadas) {
-
-    sheet2json(classificacoesPublicadas, response => {
-
-        var json = JSON.stringify(response, null, " ");
-
-        arquivoQualis = "./periodicos_conceito.json";
-
-        fs.writeFileSync(arquivoQualis, json, (err) => {
-            if (err) {
-                console.error(err)
-                return
-            }
-        })
-    });
-}
-
-
-Publicacao.prototype.xmlToJson = function (curriculoLattes) {
-
-    var xml = fs.readFileSync(curriculoLattes, 'utf8'),
-    options = {
-        ignoreComment: true, alwaysChildren: true, compact: true, addParent: true, spaces: 2
-    },
-    json = convert.xml2json(xml, options); 
-
-    arquivoLattes = "./curriculo_lattes.json";
-
-    fs.writeFileSync(arquivoLattes, json, (err) => {
-            if(err) {
-                return console.log("Erro na criação de curriculo em json: " + err);
-            }
-        }  
-    ); 
-}
-
-
-Publicacao.prototype.informaProducao = function(anoInicial, anoFinal) {
-
-    jsonfile.readFile(arquivoLattes, function (err, obj) {
-
-        if (err) console.error("Erro na leitura de curriculo em json: " + err)
-        artigos = obj['CURRICULO-VITAE']['PRODUCAO-BIBLIOGRAFICA']['ARTIGOS-PUBLICADOS']['ARTIGO-PUBLICADO'];
-        
-        retornaArtigosNoIntervalo(artigos, anoInicial, anoFinal);
-        verificaQuantidadeArtigos();
-    });
+Publicacao.prototype.informaProducao = function (jsonLattesObj, anoInicial, anoFinal) {
+    
+    artigos = obtemArtigosLattes(jsonLattesObj);
+    obtemArtigosNoIntervalo(artigos, anoInicial, anoFinal);
+    // return ... lista de produções
 }
 
 
 Publicacao.prototype.salvaProducao = function() {
 
-    jsonfile.readFile(arquivoQualis, function (err, dadosQualis) {
-
-        if (err) console.error("Erro na leitura dos periodicos em json: " + err)
-        verificaISSN(dadosQualis);
-        verificaSeArquivoFoiCriado();
-    });        
+    verificaISSN(jsonQualisObj);
+    verificaSeArquivoFoiCriado();
 }
 
 
-function retornaArtigosNoIntervalo(artigos, anoInicial, anoFinal) {
+obtemArtigosLattes = function(jsonLattesObj) {
+
+    return jsonLattesObj['CURRICULO-VITAE']['PRODUCAO-BIBLIOGRAFICA']['ARTIGOS-PUBLICADOS']['ARTIGO-PUBLICADO'];
+}
+
+
+obtemArtigosNoIntervalo = function (artigos, anoInicial, anoFinal) {
 
     var artigo = {};
 
@@ -117,10 +82,12 @@ function retornaArtigosNoIntervalo(artigos, anoInicial, anoFinal) {
             cont++;
         }
     }    
+
+    verificaQuantidadeArtigos(artigos);
 }
 
 
-function verificaQuantidadeArtigos() {
+verificaQuantidadeArtigos = function (artigos) {
 
     if (artigos.length === cont) {
         console.log("Não há artigos para o intervalo informado.")
@@ -128,11 +95,12 @@ function verificaQuantidadeArtigos() {
 }
 
 
-function verificaISSN(dadosQualis) {
+verificaISSN = function (dadosQualis) {
 
     var  periodico = {}, objArtigo = {};
 
     for (var i in dadosArtigo) {
+
         for (var j in dadosQualis) {
 
             periodico.issn = dadosQualis[j]['ISSN'];
