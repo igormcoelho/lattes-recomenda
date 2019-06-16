02b5587bc9e5079e759cdb93647e3c5b88a21b3e
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-06-16 15:00:00 -0300

    Ajustes Conferências

--- Modulos/Conferencias.js
+++ Modulos/Conferencias.js
@@ -1,11 +1,10 @@
-const fs = require('fs');
 
-
-const Parse = require('./ParseData');
 const dados = require('./Dados');
 
-
-exports = module.exports.AvaliacaoConferencia = AvaliacaoConferencia
+module.exports = {
+    AvaliacaoConferencia: AvaliacaoConferencia,
+    cruzaDadosEve: cruzaDadosEve
+}    
 
 
 function AvaliacaoConferencia(config, callback) {
@@ -16,27 +15,75 @@ function AvaliacaoConferencia(config, callback) {
 
 function Conferencia(config, callback) {
 
+    const Parse = require('./ParseData');
+    
     let parse = new Parse();
 
     let jsonLattesObj = parse.parseXmlToJson(config.curriculoLattes, callback);
-        
     let conferenciasLattes = dados.retornaLattesEventos(jsonLattesObj);
-    
     let conferenciasQualis = dados.retornaJsonObj("../Arquivos/qualis_eventos_cc_2016.json");
 
-    dados.cruzaDadosEve(conferenciasLattes, conferenciasQualis, config.anoInicial, config.anoFinal, config.similaridade, 'conferencia');
-
-    // this.verificaArquivosCriados();
+    dados.retornaNomePesquisador(jsonLattesObj);
+    cruzaDadosEve(conferenciasLattes, conferenciasQualis.conferencias, config.anoInicial, config.anoFinal, config.similaridade, 'conferencia');
 }
 
 
-Conferencia.prototype.verificaArquivosCriados = function() {
+function cruzaDadosEve(conferenciasLattes, conferenciasQualis, anoInicial, anoFinal, similaridade, origem) {
 
-    if (fs.existsSync("./resultado_conferencias_encontradas.txt")) {
-        console.log("Arquivo com conferências encontradas criado."); 
+    let stringSimilarity = require('string-similarity');
+    let conferenciaLattes = {}, conferenciaQualis = {}, conferenciasEncontradas = [], conferenciasNaoEncontradas = [], qualis = [];
+    let flag, cont;
+
+    for ( var i in conferenciasLattes ) {  
+        
+        dados.getInfosConferenciaLattes(conferenciaLattes, conferenciasLattes[i]);
+
+        if ( conferenciaLattes.anoTrabalho >= anoInicial && conferenciaLattes.anoTrabalho <= anoFinal ) {
+
+            cont = 0; flag = false; 
+            var maiorSimilaridade = 0, conferencia = {};
+
+            for ( var j in conferenciasQualis ) {
+
+                dados.getInfosConferenciaQualis(conferenciaQualis, conferenciasQualis[j]);
+                
+                var resultadoSimilaridade = stringSimilarity.compareTwoStrings(conferenciaLattes.nome, conferenciaQualis.nome); 
+                
+                if ( resultadoSimilaridade >= similaridade ) {
+
+                    if ( resultadoSimilaridade > maiorSimilaridade ) maiorSimilaridade = resultadoSimilaridade;
+
+                    dados.preencheObjConferencia(conferencia, conferenciaLattes, conferenciaQualis, maiorSimilaridade);
+                    flag = true;
+                } 
+                
+                cont++;
+                
+                dados.salvaConferenciasNaoEncontradas(conferenciasNaoEncontradas, conferenciaLattes, conferenciasQualis, cont, flag, resultadoSimilaridade);
+            }      
+
+            if ( conferencia.similaridade ) {
+
+                conferenciasEncontradas.push(conferencia);
+                qualis.push(conferencia.qualis);
+                // if ( origem == 'conferencia' ) dados.escreveJsonObj("./resultado_conferencias_encontradas.json", conferenciasEncontradas);
+            }
+        }
     }
 
-    if (fs.existsSync("./resultado_conferencias_nao_encontradas.txt")) {
-        console.log("Arquivo com conferências não encontradas criado."); 
+    if ( conferenciasEncontradas.length == 0 && conferenciasNaoEncontradas.length == 0 ) {
+
+        if ( origem == 'indice' ) {
+            console.log('Não há conferências publicadas nos últimos quatro anos.');
+        } else {
+            console.log('Não há conferências para o intervalo informado.');
+        }
+
+    } else { 
+
+        dados.verificaLista(conferenciasEncontradas, 'CONFERÊNCIAS ENCONTRADAS');
+        dados.verificaLista(conferenciasNaoEncontradas, 'CONFERÊNCIAS NÃO ENCONTRADAS');
     }
+
+    if ( origem == 'indice' ) return qualis;
 }
